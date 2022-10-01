a284bc4e97d6b3d221aa35ef427c6b890bf84bf2
olegmarkelov <vvlrff@mail.ru>
2022-10-01 13:24:48 +0300

    up

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import InputComponent from './components/InputComponent';
+
+function App() {
+
+  return (
+    <div className="container">
+      <InputComponent />
+    </div>
+  );
+}
+
+export default App;
